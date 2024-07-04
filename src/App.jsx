8051f1b543a9943c9a8cbead0f7664bd4683b51c
hemanthkanna <hemanthkanna20@gmail.com>
2024-07-04 11:47:40 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { UserProfile } from "./components/UserProfile";
 import "./global.css";
 
 export default function App() {
+  //NOT_VERIFIED | VERIFIED | DISABLED
+  const USER_STATUS = "VERIFIED";
+
   const mockUsers = [
     {
       id: "1",
@@ -25,13 +28,37 @@ export default function App() {
       email: "john@gmail.com",
     },
   ];
-  return (
-    <div>
-      <h1>Root Component</h1>
 
-      {mockUsers.map((user) => {
-        return <UserProfile key={user.id} user={user} />;
-      })}
-    </div>
-  );
+  //NOTE: Alternatively we could use switch case instead of If else
+  if (USER_STATUS === "NOT_VERIFIED") {
+    return (
+      <div>
+        <h1>User Verification Required</h1>
+        <p>Please verify your account to access the app.</p>
+      </div>
+    );
+  } else if (USER_STATUS === "VERIFIED") {
+    return (
+      <div>
+        <h1>User Verification success . Welcome</h1>
+        {mockUsers.map((user) => {
+          return <UserProfile key={user.id} user={user} />;
+        })}
+      </div>
+    );
+  } else if (USER_STATUS === "DISABLED") {
+    return (
+      <div>
+        <h1>User Account Disabled</h1>
+        <p>Your account has been disabled. Please contact support.</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>An Error Occurred</h1>
+        <p>An unexpected error occurred. Please try again later.</p>
+      </div>
+    );
+  }
 }
